7bd83ddd2e2c6b38c4824fd75afe47b92a85fb46
creamgod45 <fuyin1054@gmail.com>
2019-02-26 14:44:40 +0800

    Updata

--- _assets/js/Notification.js
+++ _assets/js/Notification.js
@@ -1,12 +1,20 @@
 
     /*
+     * @Name: Notification-js
+     * @Type: Javascript Library
+     * @Author: CreamGod45
+     * @Lang: JavaScript
+    */
+
+    /*
      *
-     *
+     * notice_api() push messages to windows Notification Box
      * 
+     * @param string $_title Title
+     * @param string $_body  Body
      * 
+     * @return bool 
      * 
-     * 
-     *  
      */
 
      function notice_api(_$title,_$body){
@@ -14,5 +22,9 @@
             Notification.requestPermission(function(_status) {
                 var n = new Notification(_$title, { body: _$body }); 
             });
+
+            return true;
+        }else{
+            return false;
         }
      }
